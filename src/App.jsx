a5b2c4173af6646b71553f166847782b6aff93bd
Yalalt <yalaltgandush@gmail.com>
2023-06-10 19:32:42 +0800

    Hero banner heseg oruulaw

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from './assets/react.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchDataFromApi } from './utils/api';
 
 import { useSelector, useDispatch } from "react-redux";
@@ -18,27 +18,40 @@ import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
   const dispatch = useDispatch();
-  const url = useSelector((state) => state.home);
+  const { url } = useSelector((state) => state.home);
   console.log(url);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi('/movie/popular')
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration')
       .then((res) => {
         console.log(res);
-        dispatch(getApiConfiguration(res));
+
+        const url = {
+          backdrop: res.images.secure_base_url + "original",
+          poster: res.images.secure_base_url + "original",
+          profile: res.images.secure_base_url + "original",
+      };
+
+        dispatch(getApiConfiguration(url));
       })
   }
 
   return (
-    <div className='App'>
-      App
-      <img src={reactLogo} alt='ReactLogo' />
-      {url?.total_pages}
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:mediaType/:id' element={<Details />} />
+        <Route path='/search/:query' element={<SearchResult />} />
+        <Route path='/explore/:mediaType' element={<Explore />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   )
 }
 
